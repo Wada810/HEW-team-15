4290a0a1623663af4217158ac4e986c5f7c6830a
sano <sano.soichiro01@gmail.com>
2022-02-22 23:22:46 +0900

    佐野進捗（指示書閲覧・正誤判定） 2022/02/22 23:20

--- tpl/view_direction.php
+++ tpl/view_direction.php
@@ -8,6 +8,7 @@
     <title>指示書確認</title>
 </head>
 <body>
+    <h1>指示書確認</h1>
     <form method="POST" action="./view_direction.php">
         <button class="" type="submit" name="status" value="back">やりなおす</button>
         <button class="" type="submit" name="status" value="complete">完成</button>
