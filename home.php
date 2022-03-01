63f0935a8b0e23d31efedc5603f197f4f7096ef2
haruna <hrn214.s226@outlook.jp>
2022-03-01 10:55:01 +0900

    ホーム追加

--- home.php
+++ home.php
@@ -1,36 +1,39 @@
 <?php 
-$loginid = $_COOKIE['loginid'];//クッキーで送られてきたログインID受け取る
-require_once 'const.php';
-
-/* データベースからニックネームを取ってくる */
-$db = new PDO('mysql:dbname=hew2022;host=localhost;charset=utf8','root','');
-
-$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
-
-$result = $db -> query("SELECT user_name FROM user WHERE id =  $loginid ; ");
-
-$list = [];
-while($row = $result->fetch(PDO::FETCH_ASSOC)){
-    $list[] = $row ;
-}
-if($list == []){
-    $name = 'ゲスト';
-}
-else{
-    foreach($list as $info){
-        $name = $info['user_name'];
-    }
-}
-var_dump($list);
-$db = null;
-
-
-
-
 
 /* ログインされていたら */
+if(isset($_COOKIE['login_id'])){
+    $login_id = $_COOKIE['login_id'];//クッキーで送られてきたログインID受け取る
+    require_once 'const.php';
 
-/* ログインされてなかったら */
+    /* データベースからニックネームを取ってくる */
+    $db = new PDO('mysql:dbname=hew2022;host=localhost;charset=utf8','root','');
+
+    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
+
+    $result = $db -> query("SELECT user_name FROM user WHERE id =  $login_id ; ");
+
+    $list = [];
+    while($row = $result->fetch(PDO::FETCH_ASSOC)){
+        $list[] = $row ;
+    }
+    // $name = $list[0]['user_name'];  ←名前表示させるのであれば表示
+    $img_file = '';//ユーザーが選択した画像src
+    $db = null;
+}
+else{ //ログインされてなかったら
+    // $name = 'ゲスト';  ←名前表示させるのであれば表示
+    $img_file = 'img/guest.png';//ゲスト用の画像src
+}
+
+
+//遊び方を見るを押されたら
+//ゲームで遊ぶを押されたら
+//みんなの指示書を見るを遊ぶを押されたら
+//プロフィールを押されたら
+
+
+
+
 
 
 require_once 'tpl/home.php';
--- tpl/home.php
+++ tpl/home.php
@@ -1,21 +1,37 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="css/foundation/destyle.css">
+    <!-- homeのCSS -->
+    <link rel="stylesheet" type="text/css" href="css/home.css">
     <title>HOME</title>
 </head>
 <body>
-<!-- ログインされていたら表示（ログインされてなかったらこっち表示） -->
-<p><img src="" alt="アイコン画像(固定のアイコン画像)"></p>
-<p><?php echo $name; ?>さん</p>
+    <div class="home">ホーム</div><!-- ヘッダーとなるところ -->
+<form method="post" action="home.php">
+    <div class="content_box">
 
-    <button type ="submit" name="" value="">ゲームで遊ぶ</button>
-    <button type ="submit" name="" value="">みんなの指示書を見る</button>
-    <button type ="submit" name="" value="">プロフィール</button>
+        <div class="user_profile">
+            <a href="#"><!-- プロフィール画面へ -->
+                <p><img src="<?php echo $img_file ;?>" alt="アイコン画像(固定のアイコン画像)"></p>
+                <!-- <?php /* echo $name; */ ?>さん 名前表示させるのであればコメントアウト外してよし-->
+                <p>プロフィール</p>
+            </a>
+        </div>
+        
+        <h1>タイトル</h1>
 
+        <div class="btn_box">
+            <div><button class="menu_btn" type ="submit" name="game_start" value="">ゲームスタート</button></div>
+            <button class="menu_btn" type ="submit" name="everyone_instructions" value="">みんなの指示書</button>
+            <div><button class="menu_btn" type ="submit" name="rule" value="">遊び方</button></div>
+        </div>
+
+    </div>
+</form>
 </body>
 </html>
