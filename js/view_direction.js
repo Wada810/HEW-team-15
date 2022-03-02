afb75f45519a0afe9b5fc759a9afa3ff97238619
sano <sano.soichiro01@gmail.com>
2022-03-02 23:36:15 +0900

    2022_03_03

--- js/view_direction.js
+++ js/view_direction.js
@@ -1,4 +1,4 @@
-// 複数処理のアコーディオン処理
+/* // 複数処理のアコーディオン処理
 const rep = document.querySelectorAll('.rep');
 
 function toggle() {
@@ -9,10 +9,10 @@ function toggle() {
 
 for (let i = 0; i < rep.length; i++) {
     rep[i].addEventListener("click", toggle);
-}
+} */
 
 
-// ○×処理
+/* // ○×処理
 const judge = document.querySelectorAll('.judge');
 
 const correct = document.querySelectorAll('.correct');
@@ -54,5 +54,147 @@ for (let j = 0; j < order.length; j++) {
             judge[l].style.display = 'none';
         }
         judge[j].style.display = 'block';
-    })
+    });
 }
+ */
+
+
+
+// リスト切り替え
+
+const up_arrow = document.getElementsByClassName('up')[0];
+const down_arrow = document.getElementsByClassName('down')[0];
+const order = document.querySelectorAll('.order');
+const first_order_val = document.getElementsByClassName('first_order_val')[0];
+const second_order_val = document.getElementsByClassName('second_order_val')[0];
+const active_order_val = document.getElementsByClassName('active_order_val')[0];
+const unopen = document.getElementsByClassName('unopen')[0];
+const first_number = document.getElementsByClassName('first_number')[0];
+const second_number = document.getElementsByClassName('second_number')[0];
+const active_number = document.getElementsByClassName('active_number')[0];
+
+active_order_val.innerHTML = order[0].innerHTML;
+active_number.innerHTML = 1;
+
+up_arrow.addEventListener('click',function (event) {
+    for(let i = 0; i < order.length; i++){
+        if(active_order_val.innerHTML == order[i].innerHTML){
+            if(active_order_val.innerHTML != order[0].innerHTML){
+                active_order_val.innerHTML = '';
+                active_number.innerHTML = '';
+                active_number.innerHTML = i;
+                active_order_val.innerHTML = order[i - 1].innerHTML;
+                second_order_val.innerHTML = '';
+                second_number.innerHTML = '';
+                if(i != 1){
+                    let insert_second_order_val = order[i - 2].innerHTML;
+                    second_order_val.innerHTML = insert_second_order_val;
+                    first_order_val.innerHTML = '';
+                    second_number.innerHTML = i;
+                    first_number.innerHTML = '';
+                    second_number.innerHTML = i - 1;
+                    if(i != 2){
+                        let insert_first_order_val = order[i - 3].innerHTML;
+                        first_order_val.innerHTML = insert_first_order_val;
+                        first_number.innerHTML = i - 2;
+                    }
+                }
+            }
+            break;
+        }
+    }
+    if(active_order_val.innerHTML == order[order.length - 1].innerHTML){
+        unopen.style.display = 'none';
+    }else{
+        unopen.style.display = 'block';
+    }
+});
+down_arrow.addEventListener('click',function (event) {
+    for(let i = 0; i < order.length; i++){
+        if(active_order_val.innerHTML == order[i].innerHTML){
+            if(active_order_val.innerHTML != order[order.length - 1].innerHTML){
+                second_order_val.innerHTML = '';
+                second_order_val.innerHTML = order[i].innerHTML;
+                second_number.innerHTML = '';
+                second_number.innerHTML = i + 1;
+                active_order_val.innerHTML = '';
+                active_order_val.innerHTML = order[i + 1].innerHTML;
+                active_number.innerHTML = '';
+                active_number.innerHTML = i + 2;
+                if(i != 0){
+                    first_order_val.innerHTML = '';
+                    first_order_val.innerHTML = order[i - 1].innerHTML;
+                    first_number.innerHTML = '';
+                    first_number.innerHTML = i;
+                }
+            }
+            break;
+        }
+    }
+    if(active_order_val.innerHTML == order[order.length - 1].innerHTML){
+        unopen.style.display = 'none';
+    }else{
+        unopen.style.display = 'block';
+    }
+});
+
+if(active_order_val.innerHTML == order[order.length - 1].innerHTML){
+    unopen.style.display = 'none';
+}else{
+    unopen.style.display = 'block';
+}
+
+// modal（一覧）
+const list_modal = document.getElementsByClassName('all_list')[0];
+const view_home = document.getElementById('view_home');
+const all_modal = document.getElementById('all_modal');
+const back_button = document.getElementsByClassName('back_button')[0];
+let flg = 0;
+
+list_modal.addEventListener('click',function (event2) {
+    view_home.style.display = 'none';
+    all_modal.style.display = 'block';
+    flg = 1;
+});
+
+back_button.addEventListener('click',function (event3) {
+    if(flg == 1){
+        view_home.style.display = 'block';
+        all_modal.style.display = 'none';
+        flg = 0;
+    }else if(flg == 2){
+        judge_modal.style.display = 'none';
+        theme_cover.style.display = 'block';
+        flg = 0;
+    }else if(flg == 3){
+        judge_modal.style.display = 'none';
+        button_modal.style.display = 'none';
+        theme_cover.style.display = 'block';
+        flg = 0;
+    }
+});
+
+
+// modal（結果）
+const com_button = document.getElementsByClassName('com_button')[0];
+const judge_modal = document.getElementById('judge_modal');
+const button_modal = document.getElementById('button_modal');
+const no_judge_button = document.getElementsByClassName('judge_button')[0];
+const ok_judge_button = document.getElementsByClassName('judge_button')[1];
+const theme_cover = document.getElementsByClassName('theme_cover')[0];
+
+com_button.addEventListener('click',function () {
+    judge_modal.style.display = 'block';
+    theme_cover.style.display = 'none';
+    flg = 2;
+})
+
+no_judge_button.addEventListener('click',function () {
+    button_modal.style.display = 'block';
+    flg = 3;
+})
+ok_judge_button.addEventListener('click',function () {
+    button_modal.style.display = 'block';
+    flg = 3;
+})
+
--- tpl/view_direction.php
+++ tpl/view_direction.php
@@ -4,41 +4,93 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/style.scss">
     <link rel="stylesheet" href="./css/view_direction.css">
     <link rel="stylesheet" href="./css/foundation">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
     <title>指示書確認</title>
 </head>
 <body>
     <header>
-        <h1>指示書確認</h1>
+        <button class="back_button" type="button">戻る</button>
+        <h1>指令を登録</h1>
     </header>
     <main>
-        <p class="theme">テーマ</p>
-        <div class="view">
-            <ol class="view_list">
-                <div class="order"><li class="">リンゴをとる</li><div class="judge"><p class="correct">○</p><p class="incorrect">×</p></div></div>
-                <div class="order rep"><li class="">リンゴを切る1</li><div class="judge"><p class="correct">○</p><p class="incorrect">×</p></div></div>
-                <div class="rep_box">
-                    <ul>
-                        <li class="rep_next">リンゴを切る2</li>
-                        <li class="rep_next">リンゴを切る3</li>
-                        <li class="rep_next">リンゴを切る4</li>
-                    </ul>
+        <div id="view_home">
+            <div class="theme_cover"><p class="theme">ピーナッツバターサンドを作る！</p></div>
+            <div class="view">
+                <div class="sub_order_cover">
+                    <div class="first_order sub_order"><p class="first_number"></p><p class="first_order_val sub_val"></p></div>
+                    <div class="second_order sub_order"><p class="second_number"></p><p class="second_order_val sub_val"></p></div>
                 </div>
-                <div class="order"><li class="">リンゴを食べる</li><div class="judge"><p class="correct">○</p><p class="incorrect">×</p></div></div>
-                <p class="unopen">・・・</p>
-            </ol>
+                <p class="up button"><span class="material-icons-round">expand_less</span></p>
+                <div class="active_order"><div class="active_cover"><p class="active_number number"></p><p class="active_order_val"></p></div></div>
+                <p class="down button"><span class="material-icons-round">keyboard_arrow_down</span></p>
+                <ul class="view_list">
+                    <li class="order">パンを2まいとる</li>
+                    <li class="order">バターナイフをとる</li>
+                    <li class="order">ピーナッツバターをぬりぬりぬりぬりぬりぬりぬりぬりぬりぬりぬりぬり</li>
+                    <li class="order">バターナイフをとる1</li>
+                    <li class="order">バターナイフをとる2</li>
+                    <li class="order">バターナイフをとる3</li>
+                    <li class="order">バターナイフをとる4</li>
+                </ul>
+                <div class="sub_action">
+                    <p class="unopen">・・・</p>
+                    <p class="all_list">一覧を見る</p>
+                </div>
+                <button class="com_button" type="button">結果へ</button>
+            </div>
         </div>
-        <form method="POST" action="./view_direction.php" class="form">
-            <div class="com_button_cover"><button class="complete_button" type="submit" name="status" value="complete">完成</button></div>
-        </form>
+        <div id="all_modal">
+            <ul class="m_view_list">
+                <li class="list_order">パンをにまいとる</li>
+                <li class="list_order">パンをにまいとる</li>
+                <li class="list_order">パンをにまいとる</li>
+                <li class="list_order">パンをにまいとる</li>
+                <li class="list_order">パンをにまいとる</li>
+                <li class="list_order">パンをにまいとる</li>
+                <li class="list_order">パンをにまいとる</li>
+                <li class="list_order">パンをにまいとる</li>
+                <li class="list_order">パンをにまいとる</li>
+                <li class="list_order">パンをにまいとる</li>
+                <li class="list_order">パンをにまいとる</li>
+                <li class="list_order">パンをにまいとる</li>
+                <li class="list_order">パンをにまいとる</li>
+                <li class="list_order">パンをにまいとる</li>
+                <li class="list_order">パンをにまいとる</li>
+                <li class="list_order">パンをにまいとる</li>
+                <li class="list_order">パンをにまいとる</li>
+                <li class="list_order">パンをにまいとる</li>
+                <li class="list_order">パンをにまいとる</li>
+            </ul>
+        </div>
+        <div id="judge_modal">
+            <p class="q_p">目標を達成できましたか？</p>
+            <div class="judge_modal_cover">
+                <div class="modal_theme_cover">
+                    <p class="modal_theme_view">テーマ</p>
+                    <p class="modal_theme">ピーナッツバターサンドを作る！</p>
+                </div>
+                <div class="judge_cover">
+                    <div class="no_cover">
+                        <p class="judge_button no">×</p>
+                        <p class="no_p">失敗</p>
+                    </div>
+                    <div class="ok_cover">
+                        <p class="judge_button ok">○</p>
+                        <p class="ok_p">成功</p>
+                    </div>
+                </div>
+            </div>
+            <div id="button_modal">
+                <div class="button_cover">
+                    <button class="re_button com_button" type="button">やり直す</button>
+                    <button class="end_button com_button" type="button">終了する</button>
+                </div>
+            </div>
+        </div>
     </main>
-    <footer>
-    <form method="POST" action="./view_direction.php" class="form">
-        <div class="back_button_cover"><button class="back_button" type="submit" name="status" value="back">やりなおす</button></div>
-    </form>
-    </footer>
     <script src="./js/view_direction.js"></script>
 </body>
 </html>
