7b42600fd7b57796fab411ed0e2475ceb8b1a14e
Teppei Hayakawa <88775149+aliciaetfleurs@users.noreply.github.com>
2022-03-02 17:19:08 +0900

    Read Me.

---
+++ tpl/add.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kosugi+Maru&display=swap" rel="stylesheet">
+    <!-- Rounded -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round"rel="stylesheet">
+    <link rel="stylesheet" href="../css/destyle.css">
+    <link rel="stylesheet" href="../css/add.css">
+    <title>Document</title>
+</head>
+<body>
+    <article>
+        <!--<header>
+            <nav>
+                <p><button type=""></button></p>
+                <h2>アルゴリズムを登録</h2>
+                <p><button type=""></button></p>
+            </nav>
+        </header> -->
+        <div class="header"></div>
+        <section id="algorithm">
+            <div id="algorithm_flame">
+                <h2 class="algorithm_theme">ピーナッツバターサンドを作る!ピーナッツバターサンドを作る!</h2>
+                <p class="algorithm_head">アルゴリズム</p>
+                <ol class="algorithm_list">
+                    <li>
+                        <div class="algorithm_number">1.</div>
+                        <div class="algorithm_contents"><p class="algorithm_name">パンを2まいとる</p><div class=""></div></div>
+                    </li>
+                    <li>
+                        <div class="algorithm_number">2.</div>
+                        <div class="algorithm_contents"><p class="algorithm_name">バターナイフをとる</p><div class=""></div></div>
+                    </li>
+                    <li>
+                        <div class="algorithm_number">3.</div>
+                        <div class="algorithm_contents"><p class="algorithm_name">ピーナッツバターを開ける</p><div class=""></div></div>
+                    </li> 
+                    <li>
+                        <div class="algorithm_number">4.</div>
+                        <div class="algorithm_contents is-active">
+                            <p class="algorithm_name">ピーナッツバターをぬるピーナッツバターをぬる</p>
+                            <div class="material_items">
+                                <button><span class="material-icons-round delete">delete</span>削除</button>
+                                <button><span class="material-icons-round border">border_color</span>編集</button>
+                            </div>
+                        </div>
+                    </li>
+                </ol>
+                <p class="algorithm_complete"><button type=""></button></p>
+            </div>
+            <div class="section_button"><button type="">完成!!!</button></div>
+        </section>
+        <div class="footer"></div>
+        <!-- <footer>
+            <section id="">
+                <div class="algorithm_add_textbox"><input type="text"></div>
+                <div class="algorithm_add_button"><button type=""></button></div>
+            </section>
+        </footer> -->
+    </article>
+</body>
+</html>
