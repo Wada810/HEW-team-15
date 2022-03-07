dea63cc7aec145398feef11e5daba8fc435e4fc1
Yuki Wada <75063370+Wada810@users.noreply.github.com>
2022-03-07 23:35:32 +0900

    dev

---
+++ hew/public/js/how_to.js
@@ -0,0 +1,5 @@
+//子供用を始める
+document.getElementById("howto_kids").addEventListener("click",()=>{
+    document.querySelector(".for_kids").classList.remove("hidden");
+    document.querySelector(".for_kids").classList.add("slide_in");
+})
--- hew/resources/views/how_to_play.blade.php
+++ hew/resources/views/how_to_play.blade.php
@@ -5,6 +5,7 @@
 @endsection
 
 @section('title', '遊び方')
+@section('body')
     <heade class="l_play_header p_play_header">
         <a href="{{ url()->previous()}}" class="p_play_header__back"><span class="material-icons-round">undo</span></a>
         <div class="p_play_header__title">
@@ -25,8 +26,29 @@
         <div class="btn-secondary mb p" id="howto_adults">大人用</div>
     </section>
     <section class="for_kids">
-
+        <div class="for_kids_window">
+            <img src="{{asset('img/panda.png')}}" class="kids_panda">
+            <div class="kids_image_box">
+                <img id="kids_image" src="{{asset('img/how_to_kids/1.png')}}">
+            </div>
+            <div class="kids_text_box">
+                まずは<span class="yellow">お題</span>を決めよう!
+                <div class="button_box">
+                    <div id="back_button"><span class="material-icons-round">arrow_back_ios_new</span></div>
+                    <div id="next_button">次へ</div>
+                </div>
+            </div>
+            <div class="page_dots">
+                <span class="active"></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
     </section>
     <section class="for_adults"></section>
-@section('body')
+    <script src="{{asset('js/how_to.js')}}"></script>
 @endsection
