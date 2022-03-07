8f6d317f46baebcf1b62e2640384e088c2e634cd
Yuki Wada <75063370+Wada810@users.noreply.github.com>
2022-03-07 21:02:05 +0900

    committ

--- hew/resources/views/how_to_play.blade.php
+++ hew/resources/views/how_to_play.blade.php
@@ -9,5 +9,14 @@
         <p></p>
         <a href="{{ url()->previous()}}" class="p_play_header__back"><span class="material-icons-round">undo</span></a>
     </header>
+    <section class="top">
+        <div>
+            <span class="material-icons-round">tungsten</span>
+            <p>あそびかた</p>
+        <div class="btn-secondary" id="howto_kids">こども用</div>
+        <div class="btn-secondary" id="howto_adult">大人用</div>
+    </section>
+    <section class=""></section>
+    <section class=""></section>
 @section('body')
 @endsection
